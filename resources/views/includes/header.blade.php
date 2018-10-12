43a640fbf01cb8487000092f86d4ccb59128b129
jnk2006 <jnk22@njit.edu>
2018-10-12 13:44:51 -0400

    Changes: 1.modified the heading in header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="/">Sample Laravel</a>
+        <a class="navbar-brand" href="/">The Bouffage</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
                 aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
