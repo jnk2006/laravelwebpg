6595d34e5d39388d3844c264e9577bfdb9a8c200
jnk2006 <jnk22@njit.edu>
2018-10-12 11:37:01 -0400

    Changes: 1.modified the footer as per needed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,14 +1,13 @@
-<footer class="container">
-    <div class="row">
-        <p class="col-sm-8 text-right">Click on these links to follow us!</p>
-        <ul class="col-sm-8 list-unstyled">
-            <li class="col-sm-1 pull-right">
-                <a href="#"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/facebook.svg"></a>
-            </li>
-            <li class="col-sm-1 pull-right">
-                <a href="#"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/instagram.svg"></a>
-            </li>
-
-        </ul>
+<footer>
+    <div class="container">
+        <div class="row col-sm-12 text-right">
+            <div id="copyright text-right">&copy; 2018 The Bouffage</div>
+            <div class="col-sm-12 justify-content-end">
+                <div class="col-sm-12">
+                    <a class="col-sm-1" href="#"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/facebook.svg" width="25px" height="25px"></a>
+                    <a class="col-sm-1" href="#"><img src="https://s3.amazonaws.com/codecademy-content/projects/make-a-website/lesson-4/instagram.svg" width="25px" height="25px"></a>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
