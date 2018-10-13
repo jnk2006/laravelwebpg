abe90dd4a9e2eb5db15b5d3d7e74de8e852c7fc3
jnk2006 <jnk22@njit.edu>
2018-10-13 08:34:27 -0400

    Changes: 1.modified the about.blade.php file 2.deleted the main.css file

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.default')
 @section('content')
 
+    <br><br><h4 class="col-sm-12 text-center"><strong>About The Bouffage</strong></h4>
     <section class="container">
         <div class="row">
             <div class="col-sm-6">
-                <br><br><h4 class="col-sm-6"><strong>About The Bouffage</strong></h4>
+                <h6 class="text-left"><strong>Features given for unregistered users:</strong></h6>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <ul>
+                    <li>Get the recipes you want</li>
+                    <li>View the tips</li>
+                    <li>Examine the recipes by ratings and reviews</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <p class="text-left">For further queries, E-mail <a href="#">here</a></p>
         </div>
     </section>
 
